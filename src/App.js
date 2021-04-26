2c3a41baa28d0b9503be2b132520258cdd882943
crissesoto <soto.crisse@hotmail.com>
2021-04-26 22:02:51 +0200

    added header and search input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
+import { useState } from "react";
+
+const Header = () => {
+  return (
+  <div className="header grid">
+    <h1 className="title">Gifty</h1>
+  </div>
+  );
+};
 
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (e) => {
+    const {value} = e.target;
+    setSearchTerm(value);
+
+    if (searchTerm.length > 2) {
+      setSearchTerm(value)
+      console.log(searchTerm)
+    }
+  };
+
+  const handleKeypress = (e) => {
+    const {value} = e.target;
+    if (e.key === "Enter" && value.length > 2) {
+      console.log("enster search");
+    }
+  };
+
   return (
-    <div className="App">
-      hello
+    <div className="page">
+      <Header />
+      <div className="search grid">
+        { /* Stack of images */}
+        <input 
+          className="grid-item input" 
+          placeholder="Type something"
+          value={searchTerm}
+          onChange={handleInputChange} 
+          onKeyPress={handleKeypress}
+        />
+      </div>
     </div>
   );
 }
